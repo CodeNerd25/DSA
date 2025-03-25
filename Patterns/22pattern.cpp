faed3d2f231fdf4636b9182e9e2957dd8066154c
Abhishek <abhishekbanerjee2k24@gmail.com>
2025-03-25 16:15:17 +0530

    Content

--- Patterns/22pattern.cpp
+++ Patterns/22pattern.cpp
@@ -8,26 +8,24 @@ Print the following Pattern
 
 #include<iostream>
 using namespace std;
-int main() {
-	int n;
-	cout << "Enter the range : ";
-	cin >> n;
-	int i=1;
-	while(i<=n) {
-		int j=1;
-		char space=i-1;
-		while(space) {
-			cout << " ";
-			space--;
-			j++;
-		}
-		int star=n-i+1;
-		while(j<=star) {
-			cout << "*";
-			star--;
-			j++;
-		}
-		cout << endl;
-		i++;
-	}
+int main(){
+    int n;
+    cout << "Enter the number of rows : ";
+    cin >> n;
+    int i=1;
+    while (i<=n){
+        int space = i-1;
+        while(space){
+            cout <<  " ";
+            space--;
+        }
+        int j=1;
+        int star=n-i+1;
+        while(j<=star){
+            cout <<"*" ;
+            j++;
+        }
+        cout << "\n";
+        i++;
+    }
 }
